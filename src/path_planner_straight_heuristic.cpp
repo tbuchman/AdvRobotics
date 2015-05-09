e18be41002e7d69c2d481329ce9c36b396b40b76
Petre Munteanu <clasianvmk@gmail.com>
2015-05-09 09:09:30 +0200

    tuned the straight_heuristic node to intake more settings

--- src/path_planner_straight_heuristic.cpp
+++ src/path_planner_straight_heuristic.cpp
@@ -68,8 +68,13 @@ float average_range(int index)
     if(index - (AVERAGING_STREAK >> 1) < 0 || index + (AVERAGING_STREAK >> 1) >= ranges.size())
     {
         ROS_ERROR("Error: for index = %d the %d-long-averanging cannot be applied (note: max range size = %lu)", index, AVERAGING_STREAK, ranges.size());
+        return 0.;
     }
     float sum = ranges[index];
+    if(SIMPLE_STREAK)
+    {
+        return sum;
+    }
     int limit = (AVERAGING_STREAK >> 1);
     for(int i = 1; i <= limit; ++i)
     {
@@ -91,8 +96,8 @@ int main(int argc, char **argv)
     ros::Subscriber subLaserScan     = n.subscribe("/girona500_RAUVI/multibeam", 1032, digestLaserScan);
 
     
-    bool forced_from_before = false;
     int times_forced = 0;
+    float to_turn;
     while (ros::ok())
     {
         if(ranges.size())
@@ -100,11 +105,15 @@ int main(int argc, char **argv)
             if(ranges.size() != beams)
             {
                 ROS_WARN("Skipped one laser scan due to inconsistent ranges[] size %lu with beams no. %d", ranges.size(), beams);
+                ros::spinOnce();
+                loop_rate.sleep();
+                continue;
             }
             nav_msgs::Odometry odometryCommand;
             int half_range = beams >> 1;
+            int target_index;
             float range_averaged = average_range(half_range);
-            if(range_averaged >= SAFE_DIST_WEIGHT * range_max)
+            if(range_averaged >= SAFE_DIST_WEIGHT * range_max && range_averaged <= range_max)
             {
                 odometryCommand.pose.pose.orientation.w = 1.;
                 odometryCommand.twist.twist.linear.x    = MAX_SPEED;
@@ -114,7 +123,6 @@ int main(int argc, char **argv)
                 odometryCommand.twist.twist.angular.y   = 0.;
                 odometryCommand.twist.twist.angular.z   = 0.;
                 
-                forced_from_before = false;
                 AVERAGING_STREAK = DEFAULT_AVERAGING_STREAK;
                 pubOdometry.publish(odometryCommand);
             }
@@ -122,66 +130,85 @@ int main(int argc, char **argv)
             {
                 // looking for the closest beam with convenient ranges[] value
                 float target_min = SAFE_DIST_WEIGHT * range_max;
-                float to_turn = 0.;
-                int target_index = -1;
+                to_turn = 0.;
+                target_index = -1;
                 for(int i = 1; i <= half_range - (AVERAGING_STREAK >> 1); ++i)
                 {
-                    if(average_range(half_range + i) >= target_min)
+                    float current_range_averaged = average_range(half_range + i);
+                    if(current_range_averaged >= target_min && current_range_averaged <= range_max)
                     {
                         target_index = half_range + i;
-                        forced_from_before = false;
                         break;
                     }
-                    if(average_range(half_range - i) >= target_min)
+                    current_range_averaged = average_range(half_range - i);
+                    if(current_range_averaged >= target_min && current_range_averaged <= range_max)
                     {
                         target_index = half_range - i;
-                        forced_from_before = false;
                         break;
                     }
                 }
                 if(-1 == target_index)
                 {
-                    // turning 180 degrees since the robot got stuck
+                    // then trying the extremes first
+                    
+                    
+                    // finally turning 180 degrees since the robot got stuck
                     if(times_forced > MAX_TIMES_FORCED)
                     {
                         ROS_ERROR("Error: exceeded max no. %d of 180 degrees forced turns. Quitting...", MAX_TIMES_FORCED);
                         return 1;
                     }
-
                     to_turn = PI/2.;
-                    ROS_INFO("forced to turn 180 degrees | time no. %d", times_forced);
-                    if(!forced_from_before)
+                    int rotational_limit = (int)(SECONDS_TOTURN * (double) LOOP_RATE);
+                    for(int i = 0; i < rotational_limit && ros::ok(); ++i)
                     {
-                        times_forced++;
+                        odometryCommand.pose.pose.orientation.w = 1.;
+                        odometryCommand.twist.twist.linear.x    = 0.;
+                        odometryCommand.twist.twist.linear.y    = 0.;
+                        odometryCommand.twist.twist.linear.z    = 0.;
+                        odometryCommand.twist.twist.angular.x   = 0.;
+                        odometryCommand.twist.twist.angular.y   = 0.;
+                        odometryCommand.twist.twist.angular.z   = to_turn;
+
+                        pubOdometry.publish(odometryCommand);
+                        ros::spinOnce();
+                        loop_rate.sleep();
                     }
-                    forced_from_before = true;
+                    ROS_INFO("forced to turn 180 degrees | time no. %d", times_forced++);
                 }
                 else
                 {
                     to_turn = (double) (target_index - half_range) * angle_increment;
                 }
-                odometryCommand.pose.pose.orientation.w = 1.;
                 // start braking (apply. speed 0.0 if critical safety distance is not ensured any more)
+                double speed = SPEED;
                 if(range_averaged < CRITICAL_SAFE_DIST_WEIGHT * range_max)
                 {
-                    odometryCommand.twist.twist.linear.x = 0.;
+                    speed = 0.;
                     AVERAGING_STREAK = DEFAULT_CRITICAL_AVERAGING_STREAK;
                 }
                 else
                 {
-                    odometryCommand.twist.twist.linear.x = MAX_SPEED;
                     AVERAGING_STREAK = DEFAULT_AVERAGING_STREAK;
                 }
 
-                odometryCommand.twist.twist.linear.y    = 0.;
-                odometryCommand.twist.twist.linear.z    = 0.;
-                odometryCommand.twist.twist.angular.x   = 0.;
-                odometryCommand.twist.twist.angular.y   = 0.;
-                odometryCommand.twist.twist.angular.z   = to_turn;
+                // turning
+                for(int i = 0; i < ROTATIONAL_ITERATIONS && ros::ok(); ++i)
+                {
+                    odometryCommand.pose.pose.orientation.w = 1.;
+                    odometryCommand.twist.twist.linear.x    = speed;
+                    odometryCommand.twist.twist.linear.y    = 0.;
+                    odometryCommand.twist.twist.linear.z    = 0.;
+                    odometryCommand.twist.twist.angular.x   = 0.;
+                    odometryCommand.twist.twist.angular.y   = 0.;
+                    odometryCommand.twist.twist.angular.z   = to_turn;
 
-                pubOdometry.publish(odometryCommand);
+                    pubOdometry.publish(odometryCommand);
+                    ros::spinOnce();
+                    loop_rate.sleep();
+                }
             }
-            ROS_INFO("threshold range = %f | threshold critical_range = %f | range found = %f", SAFE_DIST_WEIGHT * range_max, CRITICAL_SAFE_DIST_WEIGHT * range_max, ranges[half_range]);
+            ROS_INFO("threshold range = %f | threshold critical = %f | range found = %f | range best index = %d", SAFE_DIST_WEIGHT * range_max, CRITICAL_SAFE_DIST_WEIGHT * range_max, ranges[half_range], target_index);
         }
         else
         {
